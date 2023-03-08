da3af92c82228c1def055f19e301e04d0443ef3c
Sven Snoeck <snoeck.sven@live.be>
2023-03-08 16:24:42 +0100

    Convert valuta between euro and dollar

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php declare(strict_types=1);
+
+$error = NULL;
+$currencyFromOption = NULL;
+$currencyToOption = NULL;
+$currencyRates = array("euro"=>"1.05", "dollar"=>"0.95");
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $currencyFromOption = $_REQUEST["CurrencyFrom"];
+    $currencyToOption = $_REQUEST["CurrencyTo"];
+
+    if (array_key_exists("SwitchCurrency", $_POST)) {
+        $currencyFromOption = $_REQUEST["CurrencyTo"];
+        $currencyToOption = $_REQUEST["CurrencyFrom"];
+    }
     $currencyFrom = validateInput($_REQUEST["CurrencyFromInput"]);
-    if (empty($currencyFrom)) {
-        echo "oeps";
+    $currencyFrom = (float)$currencyFrom;
+
+    if ($currencyFrom == 0) {
+        $error = "&lt;&lt; Please give an amount to convert!";
+    } else {
+        $currencyTo = $currencyRates[$currencyToOption];
+        $convertedCurrency = round($currencyFrom * $currencyTo, 2);
     }
 }
 
@@ -10,6 +28,7 @@ function validateInput($inputData) {
     $inputData = trim($inputData);
     $inputData = stripslashes($inputData);
     $inputData = htmlspecialchars($inputData);
+    $inputData = str_replace(',', '.', $inputData);
     return $inputData;
 }
 
@@ -27,17 +46,20 @@ function validateInput($inputData) {
 <h1>Valuta Converter</h1>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
     <label for="CurrencyFrom">From:</label>
-    <input type="text" name="CurrencyFromInput" id="CurrencyFromInput">
+    <input type="text" name="CurrencyFromInput" id="CurrencyFromInput" value="<?php echo !isset($currencyFrom)? '' : $currencyFrom ?>">
+    <?php echo $error ?>
     <select name="CurrencyFrom" id="CurrencyFrom">
-        <option value="dollar">Dollar</option>
+        <option value="euro" <?php if ($currencyFromOption == "euro") { echo "selected"; } ?>>Euro</option>
+        <option value="dollar" <?php if ($currencyFromOption == "dollar") { echo "selected"; } ?>>Dollar</option>
     </select>
-    <button id="SwitchCurrency" value="<->">&lt;-&gt;</button>
+    <button name="SwitchCurrency" id="SwitchCurrency" value="<->">&lt;-&gt;</button>
     <label for="CurrencyTo">To:</label>
-    <input type="text" name="CurrencyToDisplay" id="CurrencyToDisplay">
+    <input type="text" name="CurrencyToDisplay" id="CurrencyToDisplay" value="<?php echo !isset($convertedCurrency) ? '' : $convertedCurrency ?>" disabled />
     <select name="CurrencyTo" id="CurrencyTo">
-        <option value="euro">Euro</option>
+        <option value="euro" <?php if ($currencyToOption == "euro") { echo "selected"; } ?>>Euro</option>
+        <option value="dollar" <?php if ($currencyToOption == "dollar") { echo "selected"; } ?>>Dollar</option>
     </select>
-    <input type="submit" value="Convert">
+    <input type="submit" value="Convert" />
 </form>
     
 </body>
